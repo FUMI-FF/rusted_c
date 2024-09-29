749da4215c5fc4e5a117a2ee1c2b1c7020029b7e
fumi <expugnatiomundi@gmail.com>
2024-09-29 16:14:57 +0900

    compile a single number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input: i32 = args[1].parse::<i32>().unwrap();
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", input);
+    println!("  ret");
 }
