f435981bd7cede2758c3395d65bff2f6f821882e
fumi <expugnatiomundi@gmail.com>
2024-12-31 09:30:03 +0900

    refactor

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,5 @@
-use crate::ast_parser::Node;
-use crate::ir::{gen_ir, IR};
+use crate::ir::IR;
 use crate::register::RegAllocator;
-use crate::register::RegIndex;
 use std::collections::HashMap;
 
 pub struct ProgramEnvironment<'a> {
@@ -13,7 +11,7 @@ pub struct ProgramEnvironment<'a> {
 }
 
 impl ProgramEnvironment<'_> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             reg_alloc: RegAllocator::new(1), // HACK: first register is used for basereg storing
             vars: HashMap::new(),
@@ -25,86 +23,7 @@ impl ProgramEnvironment<'_> {
 }
 
 // code generator
-pub fn generate<'a>(node: &Node) {
-    let mut env = ProgramEnvironment::new();
-    let ir_vec = gen_ir(node, &mut env);
-    let ir_vec = allocate_register(ir_vec, &mut env);
-    gen_x86(ir_vec, &mut env);
-}
-
-fn allocate_register(ir_vec: Vec<IR>, env: &mut ProgramEnvironment) -> Vec<IR> {
-    ir_vec
-        .iter()
-        .map(|ir| match ir {
-            IR::IMM { reg, val } => {
-                let reg: RegIndex = env.reg_alloc.allocate(*reg);
-                IR::IMM { reg, val: *val }
-            }
-            IR::MOV { dst, src } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                let src: RegIndex = env.reg_alloc.allocate(*src);
-                IR::MOV { dst, src }
-            }
-            IR::ADD { dst, src } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                let src: RegIndex = env.reg_alloc.allocate(*src);
-                IR::ADD { dst, src }
-            }
-            IR::AddImm { dst, val } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                IR::AddImm { dst, val: *val }
-            }
-            IR::SUB { dst, src } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                let src: RegIndex = env.reg_alloc.allocate(*src);
-                IR::SUB { dst, src }
-            }
-            IR::MUL { dst, src } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                let src: RegIndex = env.reg_alloc.allocate(*src);
-                IR::MUL { dst, src }
-            }
-            IR::DIV { dst, src } => {
-                let dst: RegIndex = env.reg_alloc.allocate(*dst);
-                let src: RegIndex = env.reg_alloc.allocate(*src);
-                IR::DIV { dst, src }
-            }
-            IR::LOAD { reg, addr } => {
-                let reg: RegIndex = env.reg_alloc.allocate(*reg);
-                let addr: RegIndex = env.reg_alloc.allocate(*addr);
-                IR::LOAD { reg, addr }
-            }
-            IR::STORE { addr, reg } => {
-                let reg: RegIndex = env.reg_alloc.allocate(*reg);
-                let addr: RegIndex = env.reg_alloc.allocate(*addr);
-                IR::STORE { addr, reg }
-            }
-            IR::ALLOCA { basereg, offset } => {
-                let basereg: RegIndex = env.reg_alloc.allocate(*basereg);
-                IR::ALLOCA {
-                    basereg,
-                    offset: *offset,
-                }
-            }
-            IR::RETURN(reg) => {
-                let reg = env.reg_alloc.allocate(*reg);
-                IR::RETURN(reg)
-            }
-            IR::KILL(reg) => {
-                env.reg_alloc.kill(*reg);
-                IR::NOP
-            }
-            IR::NOP => IR::NOP,
-            IR::LABEL(label) => IR::LABEL(*label),
-            IR::UNLESS { reg, label } => {
-                let reg = env.reg_alloc.allocate(*reg);
-                IR::UNLESS { reg, label: *label }
-            }
-        })
-        .collect()
-}
-
-fn gen_x86(ir_vec: Vec<IR>, env: &mut ProgramEnvironment) {
+pub fn gen_x86(ir_vec: Vec<IR>, env: &mut ProgramEnvironment) {
     let label = ".Lend";
 
     println!("  push rbp");
--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,25 @@ mod register;
 mod tokenizer;
 
 use ast_parser::ast_parser;
-use codegen::generate;
+use codegen::gen_x86;
+use codegen::ProgramEnvironment;
+use ir::gen_ir;
+use register::allocate_register;
 use std::env;
 use tokenizer::tokenizer;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input: &[u8] = args[1].as_bytes();
-    let tokenizer = tokenizer();
-    let result = tokenizer.parse(input);
+    let result = tokenizer().parse(input);
     let tokens = result.expect("failed to tokenize").1;
     let node = ast_parser().parse(&tokens).expect("failed to build AST").1;
+    let mut env = ProgramEnvironment::new();
+    let ir_vec = gen_ir(&node, &mut env);
+    let ir_vec = allocate_register(ir_vec, &mut env);
 
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    generate(&node);
+    gen_x86(ir_vec, &mut env);
 }
